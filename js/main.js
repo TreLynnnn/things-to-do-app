c272fd5d687d9a554c4dda52f866a70944341107
TreLyn <trelyn.dev@gmail.com>
2022-11-10 13:07:55 -0600

    Made changes to layout and added a clock to DOM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 console.log("JavaScript file connected!");
+
+
+// document.querySelector('button').addEventListener('click', getFetch)
+
+// function getFetch(){
+//   const choice = document.querySelector('input').value
+//   const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {
+//         console.log(data)
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+// }
+
+//Live time displayed to DOM
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("dateTime");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
+
